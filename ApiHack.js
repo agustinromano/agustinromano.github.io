675f519eb10b7ec979524f542d58f872f712db42
agustinromano <48917174+agustinromano@users.noreply.github.com>
2019-06-18 11:58:25 -0400

    Add files via upload

---
+++ ApiHack.js
@@ -0,0 +1,61 @@
+'use strict';
+
+
+function getFoodInfo() {
+  $('.js-form').on('submit', function (event) {
+    const input = $("#foodSearchInput").val();
+    console.log("input: "+input);
+    event.preventDefault();
+
+    const apiKey = 'RwF2lQB673HZPRaw9tmfa7arFAiPTb8e3Q1FSMh5';
+    const url = `https://api.nal.usda.gov/ndb/search/?format=json&q=${input}&sort=n&max=25&offset=0&api_key=${apiKey} `;
+    console.log(url);
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(responseJson => displayResults(responseJson))
+  })
+}
+
+function displayResults(test) {
+  console.log(test);
+  const dataInfo = test.list.item.map(item => {
+    return Object.values(item);
+  })
+
+  table.clear();
+  table.rows.add(dataInfo);
+  table.draw();
+}
+
+let table = null;
+
+$(document).ready(function () {
+  table = $('#myTable').DataTable({
+    data : null,
+    columnDefs: [
+      {
+          targets:3,
+          render: function ( data, type, row, meta ) {
+              if(type === 'display'){
+                  data = '<a href="index.html?nbno=' + encodeURIComponent(data) + '">' + data + '</a>';
+              }
+
+              return data;
+          }
+      }
+  ],
+
+    colums : [
+      { title: "offset"},
+      { title: "group"},
+      { title: "name"},
+      { title: "ndbno" },
+      { title: "ds"},
+      { title: "manu" }
+    ]
+  })
+  getFoodInfo();
+});
+
+
---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+$(document).ready(function () {
+    table = $('#newTable').DataTable({
+      data : null,
+      colums : [
+        { title: "ndbno"},
+        { title: "name"},
+        { title: "weight"},
+        { title: "measure" },
+        { title: "nutrients"},
+      ]
+    })
+    getNutrientInfo();
+  });
+
+const urlParams = new URLSearchParams(window.location.search);
+const nbno = urlParams.get('nbno');
+
+function getNutrientInfo() {
+      const apiKey = 'RwF2lQB673HZPRaw9tmfa7arFAiPTb8e3Q1FSMh5';
+      const url = `http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${apiKey}&nutrients=205&nutrients=204&nutrients=208&nutrients=269&nbno=${nbno}`;
+      console.log(url);
+      
+      fetch(url)
+        .then(response => response.json())
+        .then(responseJson => displayNutrients(responseJson))
+};
+
+function displayNutrients(results) {
+    console.log(results);
+    const dataInfo = results.report.foods.map(foods => {
+      return Object.values(foods);
+    });
+  
+    table.clear();
+    table.rows.add(dataInfo);
+    table.draw();
+};
+  
+  let table = null;
+  
