d41e210616509f1e361eaa4361c7ad50ff734725
agustinromano <48917174+agustinromano@users.noreply.github.com>
2019-07-06 20:06:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
 function getFoodInfo() {
   $('.js-form').on('submit', function (event) {
     const input = $("#foodSearchInput").val();
-    console.log("input: " + input);
+    console.log("input: "+input);
     event.preventDefault();
 
     const apiKey = 'RwF2lQB673HZPRaw9tmfa7arFAiPTb8e3Q1FSMh5';
     const url = `https://api.nal.usda.gov/ndb/search/?format=json&q=${input}&sort=n&max=25&offset=0&api_key=${apiKey} `;
     console.log(url);
-
+    
     fetch(url)
       .then(response => response.json())
       .then(responseJson => displayResults(responseJson))
@@ -32,30 +32,28 @@ let table = null;
 
 $(document).ready(function () {
   table = $('#myTable').DataTable({
-    responsive: true,
-    data: null,
+    data : null,
     columnDefs: [
       {
-        targets: 3,
-        render: function (data, type, row, meta) {
-          if (type === 'display') {
-            data = '<a href="index2.html?nbno=' + encodeURIComponent(data) + '">' + data + '</a>';
+          targets:3,
+          render: function ( data, type, row, meta ) {
+              if(type === 'display'){
+                  data = '<a href="index.html?nbno=' + encodeURIComponent(data) + '">' + data + '</a>';
+              }
+
+              return data;
           }
-
-          return data;
-        }
       }
-    ],
+  ],
 
-    colums: [
-      { title: "offset" },
-      { title: "group" },
-      { title: "name" },
+    colums : [
+      { title: "offset"},
+      { title: "group"},
+      { title: "name"},
       { title: "ndbno" },
-      { title: "ds" },
+      { title: "ds"},
       { title: "manu" }
     ]
   })
   getFoodInfo();
 });
-
